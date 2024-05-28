d88272e7d482a0d1121e1eac6382569e42cc5222
Yil Verdeja <yilverdeja@gmail.com>
2024-05-28 16:10:43 +0800

    feat: add retry logic for in progress status

--- src/hooks/useFetchRoute.ts
+++ src/hooks/useFetchRoute.ts
@@ -2,6 +2,8 @@ import axios from 'axios';
 import { GetRouteResponse } from '../types';
 import { useCallback, useState } from 'react';
 
+const maxRetries = 5;
+
 const axiosInstance = axios.create({
 	baseURL: import.meta.env.VITE_APP_URL,
 	headers: {
@@ -18,20 +20,38 @@ const useFetchRoute = () => {
 		setRoute(null);
 		setError(null);
 		setLoading(true);
-		try {
-			const response = await axiosInstance
-				.get<GetRouteResponse>(`/route/${token}`)
-				.then((res) => res.data);
-			setRoute(response);
-			setLoading(false);
-		} catch (err) {
-			if (err instanceof Error) setError(err);
-			else setError(new Error(String(err)));
-			setRoute(null);
-			setLoading(false);
-		} finally {
-			setLoading(false);
-		}
+
+		// fetch data with retry logic
+		const fetchData = async (retryCount = 0) => {
+			try {
+				const response = await axiosInstance
+					.get<GetRouteResponse>(`/route/${token}`)
+					.then((res) => res.data);
+				if (response.status === 'in progress') {
+					if (retryCount < maxRetries) {
+						setTimeout(() => {
+							fetchData(retryCount + 1);
+						}, Math.pow(2, retryCount) * 1000); // exponential backoff to reduce server calls
+					} else {
+						throw new Error('Max retries attempts reached');
+					}
+				} else if (response.status === 'success') {
+					setRoute(response);
+					setLoading(false);
+				} else {
+					throw new Error('Failed to fetch route');
+				}
+			} catch (err) {
+				if (err instanceof Error) setError(err);
+				else setError(new Error(String(err)));
+				setRoute(null);
+				setLoading(false);
+			} finally {
+				setLoading(false);
+			}
+		};
+
+		fetchData();
 	}, []);
 
 	return { fetchRoute, route, loading, error };
